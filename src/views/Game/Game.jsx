194d69feb1799dec8f718e93eac8a5626a3dee37
Simon Davis <simonfreelance1@gmail.com>
2020-10-24 22:51:12 -0400

    Started on Game Logic

--- src/views/Game/Game.jsx
+++ src/views/Game/Game.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import {
   Clock,
   HourHand,
@@ -11,6 +11,12 @@ import {
   MinuteDigit,
   SecondDigit
 } from "../../components/Clocks/DigitalClock/DigitalClock";
+import { useLocalStorageState } from "../../hooks/useLocalStorageState";
+import {
+  isValidHour,
+  isValidMinute,
+  isValidSecond
+} from "../../components/Clocks/useClock";
 const modes = {
   EASY: "EASY",
   MEDIUM: "MEDIUM",
@@ -18,11 +24,18 @@ const modes = {
 };
 export const Game = ({ mode = modes.EASY }) => {
   const [time, setTime] = useState({
-    hour: 0,
+    hour: 12,
     minute: 0,
     second: 0
   });
+  const [sequences, setSequences] = useState([]);
 
+  const [input, setInput] = useState({
+    hour: "",
+    minute: "",
+    second: ""
+  });
+  const [isAnswerTime, setIsAnswerTime] = useState(false);
   return (
     <div
       style={{
@@ -36,12 +49,35 @@ export const Game = ({ mode = modes.EASY }) => {
         {mode === modes.MEDIUM || mode === modes.HARD ? <MinuteHand /> : null}
         {mode === modes.HARD ? <SecondHand /> : null}
       </Clock>
-      <DigitalClock hour={time.hour} minute={time.minute} second={time.second}>
+      <DigitalClock>
         <HourDigit />
         {mode === modes.MEDIUM || mode === modes.HARD ? <MinuteDigit /> : null}
         {mode === modes.HARD ? <SecondDigit /> : null}
       </DigitalClock>
-      <div style={{ display: "flex", justifyContent: "space-between" }}></div>
+      <div>
+        <p>{`What was the #${sequences.length + 1} clock's time?`}</p>
+        <div>
+          <div>
+            <label htmlFor="hour">Hour:</label>
+            <input
+              type="text"
+              id="hour"
+              value={input.hour}
+              onChange={e => {
+                if (isValidHour(e.target.value)) {
+                  setInput(prevState => {
+                    return { ...prevState, hour: e.target.value };
+                  });
+                } else if (e.target.value.length === 0) {
+                  return setInput(prevState => {
+                    return { ...prevState, hour: "" };
+                  });
+                }
+              }}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
